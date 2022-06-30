70951f01a32af0697712a4cbfce5fd82c9f1bf88
hodorovich <vladh17dmv@mail.ru>
2022-06-30 22:59:02 +0300

    buttons in dict

--- Calculator.py
+++ Calculator.py
@@ -17,14 +17,17 @@ def insert_number(value):
 
 
 def correct_number_value(number):
-    if ',' in number:
-        number = float(number.replace(',', '.'))
-        if str(number)[-1] == '0':
-            return int(str(number)[:-2])
-        else:
-            return float(number)
+    if len(number) == 0:
+        pass
     else:
-        return int(number) 
+        if ',' in number or '.' in number:
+            number = float(number.replace(',', '.'))
+            if str(number)[-1] == '0':
+                return int(str(number)[:-2])
+            else:
+                return float(number)
+        else:
+            return int(number) 
 
 
 def change_sign():
@@ -40,17 +43,28 @@ saved_number = 0
 sign_of_previous_operation = ''
 
 def do_previous_opperation(sign):
-    current_number = correct_number_value(result_of_operation.get())
-    if sign == '+':
-        result = saved_number + current_number
-    elif sign == '-':
-        result = saved_number - current_number
-    elif sign == '/':
-        result = saved_number / current_number
-    elif sign == '*':
-        result = saved_number * current_number
+    global sign_of_previous_operation
 
-    result_of_operation.set(str(result).replace('.', ','))
+    if len(result_of_operation.get()) == 0:
+        # sign_of_previous_operation = sign
+        # print(output_of_last_operation_variable.get()[:-3])
+        # new_output = f'{output_of_last_operation_variable.get()[:-3]} {sign}'
+        # print(output_of_last_operation_variable.get()[:-3])
+        # print(new_output)
+        # output_of_last_operation_variable.set(new_output)
+        # return
+        pass
+    else:
+        current_number = correct_number_value(result_of_operation.get())
+        if sign == '+':
+            result = saved_number + current_number
+        elif sign == '-':
+            result = saved_number - current_number
+        elif sign == '/':
+            result = saved_number / current_number
+        elif sign == '*':
+            result = saved_number * current_number
+        result_of_operation.set(str(result).replace('.', ','))
 
 def operation_to_add():
     global sign_of_previous_operation
@@ -105,13 +119,13 @@ def operation_to_multiply():
     global saved_number
 
     if len(sign_of_previous_operation) == 0:
-        sign_of_previous_operation = '/'
+        sign_of_previous_operation = '*'
         saved_number = correct_number_value(result_of_operation.get())
-        output_of_last_operation_variable.set(f'{saved_number} / ')
+        output_of_last_operation_variable.set(f'{saved_number} * ')
         result_of_operation.set('')
     else:
         do_previous_opperation(sign_of_previous_operation)
-        sign_of_previous_operation = '/'
+        sign_of_previous_operation = '*'
         output_of_last_operation_variable.set(f'{result_of_operation.get()} - ')
         saved_number = correct_number_value(result_of_operation.get())
         result_of_operation.set('')
@@ -163,51 +177,36 @@ result_of_operation_entry.grid(column=0, row=1, columnspan=4, sticky=E) # STICKY
 
 # Creating buttons
 # + - * % backpase clear =
-button_plus = Button(mainframe, text='+', command=lambda: operation_to_add())
-button_plus.grid(column=0, row=2, padx = 10, pady=10)
-button_minus = Button(mainframe, text='-', command=lambda: operation_to_minus())
-button_minus.grid(column=1, row=2, padx = 10, pady=10)
-button_divide = Button(mainframe, text='%', command=lambda: operation_to_divide())
-button_divide.grid(column=2, row=2, padx = 10, pady=10)
-button_multiply = Button(mainframe, text='x', command=lambda: operation_to_multiply())
-button_multiply.grid(column=3, row=2, padx = 10, pady=10)
+buttons_dict = {
+    'button_plus' : {'text' : '+', 'command': lambda: operation_to_add(), 'column': 0, 'row':2},
+    'button_minus': {'text' : '-', 'command': lambda: operation_to_minus(), 'column': 1, 'row':2},
+    'button_divide': {'text' : '/', 'command': lambda: operation_to_divide(), 'column': 2, 'row':2},
+    'button_multiply': {'text' : '*', 'command': lambda: operation_to_multiply(), 'column': 3, 'row':2},
+    'button_number_0': {'text' : '0', 'command': lambda: insert_number(0), 'column': 1, 'row':6},
+    'button_number_1': {'text' : '1', 'command': lambda: insert_number(1), 'column': 0, 'row':5},
+    'button_number_2': {'text' : '2', 'command': lambda: insert_number(2), 'column': 1, 'row':5},
+    'button_number_3': {'text' : '3', 'command': lambda: insert_number(3), 'column': 2, 'row':5},
+    'button_number_4': {'text' : '4', 'command': lambda: insert_number(4), 'column': 0, 'row':4},
+    'button_number_5': {'text' : '5', 'command': lambda: insert_number(5), 'column': 1, 'row':4},
+    'button_number_6': {'text' : '6', 'command': lambda: insert_number(6), 'column': 2, 'row':4},
+    'button_number_7': {'text' : '7', 'command': lambda: insert_number(7), 'column': 0, 'row':3},
+    'button_number_8': {'text' : '8', 'command': lambda: insert_number(8), 'column': 1, 'row':3},
+    'button_number_9': {'text' : '9', 'command': lambda: insert_number(9), 'column': 2, 'row':3},
+    'button_comma': {'text' : ',', 'command': lambda: insert_number(','), 'column': 2, 'row':6},
+    'button_plus_or_minus': {'text' : '+/-', 'command': lambda: change_sign(), 'column': 0, 'row':6},
+    'button_x_squared': {'text' : 'x^2', 'command': lambda: operation_to_square(), 'column': 3, 'row':3},
+    'button_square_root_of_x': {'text' : 'sqrt(x)', 'command': lambda: operation_to_square_root(), 'column': 3, 'row':4},
+    'button_clear': {'text' : 'Clear', 'command': lambda: operation_to_clear(), 'column': 3, 'row':5},
+    'button_equal': {'text' : '=', 'command': lambda: operation_to_equal(), 'column': 3, 'row':6},
+}
 
-button_number_0 = Button(mainframe, text='0', command=lambda: insert_number(0))
-button_number_0.grid(column=1, row=6, padx = 10, pady=10)
-button_number_1 = Button(mainframe, text='1', command=lambda: insert_number(1))
-button_number_1.grid(column=0, row=5, padx = 10, pady=10)
-button_number_2 = Button(mainframe, text='2',command=lambda: insert_number(2))
-button_number_2.grid(column=1, row=5, padx = 10, pady=10)
-button_number_3 = Button(mainframe, text='3',command=lambda: insert_number(3))
-button_number_3.grid(column=2, row=5, padx = 10, pady=10)
-button_number_4 = Button(mainframe, text='4',command=lambda: insert_number(4))
-button_number_4.grid(column=0, row=4, padx = 10, pady=10)
-button_number_5 = Button(mainframe, text='5',command=lambda: insert_number(5))
-button_number_5.grid(column=1, row=4, padx = 10, pady=10)
-button_number_6 = Button(mainframe, text='6',command=lambda: insert_number(6))
-button_number_6.grid(column=2, row=4, padx = 10, pady=10)
-button_number_7 = Button(mainframe, text='7',command=lambda: insert_number(7))
-button_number_7.grid(column=0, row=3, padx = 10, pady=10)
-button_number_8 = Button(mainframe, text='8',command=lambda: insert_number(8))
-button_number_8.grid(column=1, row=3, padx = 10, pady=10)
-button_number_9 = Button(mainframe, text='9',command=lambda: insert_number(9))
-button_number_9.grid(column=2, row=3, padx = 10, pady=10)
-
-button_comma = Button(mainframe, text=',', command=lambda: insert_number(','))
-button_comma.grid(column=2, row=6, padx = 10, pady=10)
-button_plus_or_minus = Button(mainframe, text='+/-', command=lambda:change_sign())
-button_plus_or_minus.grid(column=0, row=6, padx = 10, pady=10)
-
-button_x_squared = Button(mainframe, text='x^2', command=lambda:operation_to_square())
-button_x_squared.grid(column=3, row=3, padx = 10, pady=10)
-button_square_root_of_x = Button(mainframe, text='sqrt(x)', command=lambda:operation_to_square_root())
-button_square_root_of_x.grid(column=3, row=4, padx = 10, pady=10)
-button_clear = Button(mainframe, text='Clear', command=lambda:operation_to_clear())
-button_clear.grid(column=3, row=5, padx = 10, pady=10)
-button_equal = Button(mainframe, text='=', command=lambda:operation_to_equal())
-button_equal.grid(column=3, row=6, padx = 10, pady=10)
-
-
-
+for button in buttons_dict:
+    Button(mainframe, text = buttons_dict[button]['text'],
+    command = buttons_dict[button]['command']).grid(
+        column = buttons_dict[button]['column'],
+        row = buttons_dict[button]['row'],
+        padx = 15,
+        pady = 15
+    )
 
 root.mainloop()
