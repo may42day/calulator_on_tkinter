6e92b51ed21fbc242538ca818592e7db60638b15
hodorovich <vladh17dmv@mail.ru>
2022-07-01 18:52:46 +0300

    Finished

--- Calculator.py
+++ Calculator.py
@@ -1,8 +1,5 @@
-import numbers
 from tkinter import *
-# from tkinter import ttk
 
-# Functions
 def insert_number(value):
     current_number = result_of_operation.get()
     if value == 0 and current_number == '0' or value == ',' and ',' in current_number:
@@ -14,7 +11,6 @@ def insert_number(value):
             result_of_operation.set(current_number + ',')
     else:
         result_of_operation.set(current_number + str(value))
-
 
 def correct_number_value(number):
     if len(number) == 0:
@@ -29,7 +25,6 @@ def correct_number_value(number):
         else:
             return int(number) 
 
-
 def change_sign():
     current_number = result_of_operation.get()
     if len(current_number) == 0:
@@ -39,20 +34,10 @@ def change_sign():
     else:
         result_of_operation.set('-' + current_number)
 
-saved_number = 0
-sign_of_previous_operation = ''
-
 def do_previous_opperation(sign):
-    global sign_of_previous_operation
+    global last_operation
 
     if len(result_of_operation.get()) == 0:
-        # sign_of_previous_operation = sign
-        # print(output_of_last_operation_variable.get()[:-3])
-        # new_output = f'{output_of_last_operation_variable.get()[:-3]} {sign}'
-        # print(output_of_last_operation_variable.get()[:-3])
-        # print(new_output)
-        # output_of_last_operation_variable.set(new_output)
-        # return
         pass
     else:
         current_number = correct_number_value(result_of_operation.get())
@@ -61,152 +46,168 @@ def do_previous_opperation(sign):
         elif sign == '-':
             result = saved_number - current_number
         elif sign == '/':
-            result = saved_number / current_number
+            try :
+                result = saved_number / current_number
+            except ZeroDivisionError:
+                operation_to_clear()
+                global saved_second_number
+                return None  
         elif sign == '*':
             result = saved_number * current_number
-        result_of_operation.set(str(result).replace('.', ','))
+        elif sign == '=':
+            pass
+        if sign != 'sqr' and sign != 'sqrt':
+            result_of_operation.set(str(result).replace('.', ','))
 
-def operation_to_add():
-    global sign_of_previous_operation
+def operation_with_sign(sign):
+    global last_operation
     global saved_number
-
-    if len(sign_of_previous_operation) == 0:
-        sign_of_previous_operation = '+'
+    
+    if len(result_of_operation.get()) == 0 and len(last_operation) == 0:
+        pass  
+    elif len(last_operation) == 0:
+        last_operation = sign
         saved_number = correct_number_value(result_of_operation.get())
-        output_of_last_operation_variable.set(f'{saved_number} + ')
+        output_of_last_operation_variable.set(f'{saved_number} {sign} ')
         result_of_operation.set('')
+    elif len(result_of_operation.get()) == 0:
+        last_operation = sign
+        new_output = f'{saved_number} {sign} '
+        output_of_last_operation_variable.set(new_output)
+    elif sign == '=':
+        saved_second_number = correct_number_value(result_of_operation.get())
+        if last_operation != 'sqrt' and last_operation != 'sqr':
+            do_previous_opperation(last_operation)
+            output_of_last_operation_variable.set(f'{saved_number} {last_operation} {saved_second_number} = ')
+        else:
+            output_of_last_operation_variable.set(f'{last_operation} of {saved_number} = ')
+        last_operation = ""
+        saved_number = correct_number_value(result_of_operation.get())
     else:
-        do_previous_opperation(sign_of_previous_operation)
-        sign_of_previous_operation = '+'
-        output_of_last_operation_variable.set(f'{result_of_operation.get()} + ')
-        saved_number = correct_number_value(result_of_operation.get())
-        result_of_operation.set('')
-
-def operation_to_minus():
-    global sign_of_previous_operation
-    global saved_number
-
-    if len(sign_of_previous_operation) == 0:
-        sign_of_previous_operation = '-'
-        saved_number = correct_number_value(result_of_operation.get())
-        output_of_last_operation_variable.set(f'{saved_number} - ')
-        result_of_operation.set('')
-    else:
-        do_previous_opperation(sign_of_previous_operation)
-        sign_of_previous_operation = '-'
-        output_of_last_operation_variable.set(f'{result_of_operation.get()} - ')
-        saved_number = correct_number_value(result_of_operation.get())
-        result_of_operation.set('')
-
-def operation_to_divide():
-    global sign_of_previous_operation
-    global saved_number
-
-    if len(sign_of_previous_operation) == 0:
-        sign_of_previous_operation = '/'
-        saved_number = correct_number_value(result_of_operation.get())
-        output_of_last_operation_variable.set(f'{saved_number} / ')
-        result_of_operation.set('')
-    else:
-        do_previous_opperation(sign_of_previous_operation)
-        sign_of_previous_operation = '/'
-        output_of_last_operation_variable.set(f'{result_of_operation.get()} - ')
-        saved_number = correct_number_value(result_of_operation.get())
-        result_of_operation.set('')
-
-def operation_to_multiply():
-    global sign_of_previous_operation
-    global saved_number
-
-    if len(sign_of_previous_operation) == 0:
-        sign_of_previous_operation = '*'
-        saved_number = correct_number_value(result_of_operation.get())
-        output_of_last_operation_variable.set(f'{saved_number} * ')
-        result_of_operation.set('')
-    else:
-        do_previous_opperation(sign_of_previous_operation)
-        sign_of_previous_operation = '*'
-        output_of_last_operation_variable.set(f'{result_of_operation.get()} - ')
+        do_previous_opperation(last_operation)
+        last_operation = sign
+        output_of_last_operation_variable.set(f'{result_of_operation.get()} {sign} ')
         saved_number = correct_number_value(result_of_operation.get())
         result_of_operation.set('')
 
 def operation_to_square():
-    current_number = correct_number_value(result_of_operation.get())
-    output_of_last_operation_variable.set(f'sqr({current_number})')
-    result = str(current_number ** 2).replace('.', ',')
-    result_of_operation.set(result)
+    global last_operation
+    global saved_number
+
+    if len(last_operation) != 0:
+        do_previous_opperation(last_operation)
+    elif len(result_of_operation.get()) == 0:
+        pass
+    else:
+        saved_number = correct_number_value(result_of_operation.get())
+        output_of_last_operation_variable.set(f'{saved_number}²')
+        result = str(saved_number ** 2).replace('.', ',')
+        result_of_operation.set(result)
+        last_operation = 'sqr'
 
 def operation_to_square_root():
-    current_number = correct_number_value(result_of_operation.get())
-    output_of_last_operation_variable.set(f'sqrt({current_number})')
-    result = str(current_number ** 0.5).replace('.', ',')
-    if result[-1] == '0':
-        result_of_operation.set(result[:-2])
-    else:
-        result_of_operation.set(result)
+    global last_operation
+    global saved_number
 
-def operation_to_equal():
-    pass
+    if len(last_operation) != 0:
+        do_previous_opperation(last_operation)
+    elif len(result_of_operation.get()) == 0:
+        pass
+    else:
+        saved_number = correct_number_value(result_of_operation.get())
+        output_of_last_operation_variable.set(f'√{saved_number} =')
+        result = str(saved_number ** 0.5).replace('.', ',')
+        last_operation = 'sqrt'
+        if result[-1] == '0':
+            result_of_operation.set(result[:-2])
+        else:
+            result_of_operation.set(result)
 
 def operation_to_clear():
-    result_of_operation.set('0')
+    global saved_number
+    global last_operation
+    
+    result_of_operation.set('')
     output_of_last_operation_variable.set('')
+    saved_number = 0    
+    last_operation = ''
+
+# Colors for elements
+color_main_background = '#9da0a6'
+color_output_of_last_operation = '#9da0a6'
+color_result_of_operation = '#727987'
+color_background_of_button_number = 'white'
+color_activebackground_of_button_number = '#c2c5cb'
+color_background_of_button_sign = '#958ad7'
+color_activebackground_of_button_sign = '#b3aed1'
 
 
-# TTK module
 root = Tk()
 root.title("Calculator")
-root.geometry('300x450+100+100')
-
-
-# Creating mainframe
-mainframe = Frame(root)
-mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+root.geometry('290x345+100+100')
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
-# Creating label with output of last operation
+# Variables
+saved_number = 0    
+last_operation = '' 
 output_of_last_operation_variable = StringVar()
-output_of_last_operation_label = Label(mainframe, width = 20, textvariable=output_of_last_operation_variable)
-output_of_last_operation_label.grid(column=0, row=0, columnspan=4, sticky=E) # STICKY????
-
-# Creating label with result of calculating
 result_of_operation = StringVar()
-result_of_operation_entry = Label(mainframe, width=20, textvariable=result_of_operation)
-result_of_operation_entry.grid(column=0, row=1, columnspan=4, sticky=E) # STICKY????
 
-# Creating buttons
-# + - * % backpase clear =
+mainframe = Frame(root, background=color_main_background)
+mainframe.grid(column=0, row=0,sticky=(N, W, E, S))
+
+for column in range(4):
+    mainframe.columnconfigure(column, weight = 1)
+
+for row in range(7):
+    mainframe.rowconfigure(row, weight=1)
+
+
+output_of_last_operation_label = Label(mainframe,background=color_output_of_last_operation, textvariable=output_of_last_operation_variable, width=22, height=1, font = 'Arial 10', anchor= E)
+output_of_last_operation_label.grid(column=0, row=0, columnspan=4, padx=10, pady=5, sticky=(N, W, E, S))
+
+
+result_of_operation_label = Label(mainframe, background=color_result_of_operation, textvariable=result_of_operation, width=19, height=1, font = 'Arial 18', anchor= E)
+result_of_operation_label.grid(column=0, row=1, columnspan=4, padx=10, pady=5, sticky=(N, W, E, S))
+
 buttons_dict = {
-    'button_plus' : {'text' : '+', 'command': lambda: operation_to_add(), 'column': 0, 'row':2},
-    'button_minus': {'text' : '-', 'command': lambda: operation_to_minus(), 'column': 1, 'row':2},
-    'button_divide': {'text' : '/', 'command': lambda: operation_to_divide(), 'column': 2, 'row':2},
-    'button_multiply': {'text' : '*', 'command': lambda: operation_to_multiply(), 'column': 3, 'row':2},
-    'button_number_0': {'text' : '0', 'command': lambda: insert_number(0), 'column': 1, 'row':6},
-    'button_number_1': {'text' : '1', 'command': lambda: insert_number(1), 'column': 0, 'row':5},
-    'button_number_2': {'text' : '2', 'command': lambda: insert_number(2), 'column': 1, 'row':5},
-    'button_number_3': {'text' : '3', 'command': lambda: insert_number(3), 'column': 2, 'row':5},
-    'button_number_4': {'text' : '4', 'command': lambda: insert_number(4), 'column': 0, 'row':4},
-    'button_number_5': {'text' : '5', 'command': lambda: insert_number(5), 'column': 1, 'row':4},
-    'button_number_6': {'text' : '6', 'command': lambda: insert_number(6), 'column': 2, 'row':4},
-    'button_number_7': {'text' : '7', 'command': lambda: insert_number(7), 'column': 0, 'row':3},
-    'button_number_8': {'text' : '8', 'command': lambda: insert_number(8), 'column': 1, 'row':3},
-    'button_number_9': {'text' : '9', 'command': lambda: insert_number(9), 'column': 2, 'row':3},
-    'button_comma': {'text' : ',', 'command': lambda: insert_number(','), 'column': 2, 'row':6},
-    'button_plus_or_minus': {'text' : '+/-', 'command': lambda: change_sign(), 'column': 0, 'row':6},
-    'button_x_squared': {'text' : 'x^2', 'command': lambda: operation_to_square(), 'column': 3, 'row':3},
-    'button_square_root_of_x': {'text' : 'sqrt(x)', 'command': lambda: operation_to_square_root(), 'column': 3, 'row':4},
-    'button_clear': {'text' : 'Clear', 'command': lambda: operation_to_clear(), 'column': 3, 'row':5},
-    'button_equal': {'text' : '=', 'command': lambda: operation_to_equal(), 'column': 3, 'row':6},
+    'button_plus' : {'text' : '+', 'command': lambda: operation_with_sign('+'), 'column': 0, 'row':2, 'background':color_background_of_button_sign , 'activebackground': color_activebackground_of_button_sign},
+    'button_minus': {'text' : '-', 'command': lambda: operation_with_sign('-'), 'column': 1, 'row':2, 'background':color_background_of_button_sign , 'activebackground': color_activebackground_of_button_sign},
+    'button_divide': {'text' : '/', 'command': lambda: operation_with_sign('/'), 'column': 2, 'row':2, 'background':color_background_of_button_sign , 'activebackground': color_activebackground_of_button_sign},
+    'button_multiply': {'text' : '*', 'command': lambda: operation_with_sign('*'), 'column': 3, 'row':2, 'background':color_background_of_button_sign , 'activebackground': color_activebackground_of_button_sign},
+    'button_number_0': {'text' : '0', 'command': lambda: insert_number(0), 'column': 1, 'row':6, 'background':color_background_of_button_number , 'activebackground': color_activebackground_of_button_number},
+    'button_number_1': {'text' : '1', 'command': lambda: insert_number(1), 'column': 0, 'row':5, 'background':color_background_of_button_number , 'activebackground': color_activebackground_of_button_number},
+    'button_number_2': {'text' : '2', 'command': lambda: insert_number(2), 'column': 1, 'row':5, 'background':color_background_of_button_number , 'activebackground': color_activebackground_of_button_number},
+    'button_number_3': {'text' : '3', 'command': lambda: insert_number(3), 'column': 2, 'row':5, 'background':color_background_of_button_number , 'activebackground': color_activebackground_of_button_number},
+    'button_number_4': {'text' : '4', 'command': lambda: insert_number(4), 'column': 0, 'row':4, 'background':color_background_of_button_number , 'activebackground': color_activebackground_of_button_number},
+    'button_number_5': {'text' : '5', 'command': lambda: insert_number(5), 'column': 1, 'row':4, 'background':color_background_of_button_number , 'activebackground': color_activebackground_of_button_number},
+    'button_number_6': {'text' : '6', 'command': lambda: insert_number(6), 'column': 2, 'row':4, 'background':color_background_of_button_number , 'activebackground': color_activebackground_of_button_number},
+    'button_number_7': {'text' : '7', 'command': lambda: insert_number(7), 'column': 0, 'row':3, 'background':color_background_of_button_number , 'activebackground': color_activebackground_of_button_number},
+    'button_number_8': {'text' : '8', 'command': lambda: insert_number(8), 'column': 1, 'row':3, 'background':color_background_of_button_number , 'activebackground': color_activebackground_of_button_number},
+    'button_number_9': {'text' : '9', 'command': lambda: insert_number(9), 'column': 2, 'row':3, 'background':color_background_of_button_number , 'activebackground': color_activebackground_of_button_number},
+    'button_comma': {'text' : ',', 'command': lambda: insert_number(','), 'column': 2, 'row':6, 'background':color_background_of_button_sign , 'activebackground': color_activebackground_of_button_sign},
+    'button_plus_or_minus': {'text' : '+/-', 'command': lambda: change_sign(), 'column': 0, 'row':6, 'background':color_background_of_button_sign , 'activebackground': color_activebackground_of_button_sign},
+    'button_x_squared': {'text' : 'x²', 'command': lambda: operation_to_square(), 'column': 3, 'row':3, 'background':color_background_of_button_sign , 'activebackground': color_activebackground_of_button_sign},
+    'button_square_root_of_x': {'text' : '√x', 'command': lambda: operation_to_square_root(), 'column': 3, 'row':4, 'background':color_background_of_button_sign , 'activebackground': color_activebackground_of_button_sign},
+    'button_clear': {'text' : 'C', 'command': lambda: operation_to_clear(), 'column': 3, 'row':5, 'background':color_background_of_button_sign , 'activebackground': color_activebackground_of_button_sign},
+    'button_equal': {'text' : '=', 'command': lambda: operation_with_sign('='), 'column': 3, 'row':6, 'background':color_background_of_button_sign , 'activebackground': color_activebackground_of_button_sign},
 }
 
+# Draw all buttons
 for button in buttons_dict:
     Button(mainframe, text = buttons_dict[button]['text'],
-    command = buttons_dict[button]['command']).grid(
+    command = buttons_dict[button]['command'],
+    foreground="black", background=buttons_dict[button]['background'], 
+    width = 6, height = 2,
+    font = 'Arial 13 bold', relief = FLAT, bd = 0,
+    activebackground = buttons_dict[button]['activebackground']
+    ).grid(
         column = buttons_dict[button]['column'],
         row = buttons_dict[button]['row'],
-        padx = 15,
-        pady = 15
+        padx = 3, pady = 3,
+        sticky=(N, W, E, S)
     )
+
 
 root.mainloop()
